65aa8a717994712d26f17431548f74856b9f1ba0
Akshay Mhatre <akshay.mhatre8989@gmail.com>
2018-08-10 19:34:57 +0530

    db email id upadted

--- config.js
+++ config.js
@@ -2,7 +2,6 @@ module.exports = {
     'secret': 'MYSECRETKEY007',
     //'database':'mongodb://localhost:27017/elearnfinal89',
     'database': 'mongodb://akshay:akshay123@ds115569.mlab.com:15569/elearn',
-    //'database':'mongodb://hemant1:Hemant1@ds157469.mlab.com:57469/elearndb',
     //'PORT1':process.env.PORT || 3001
      'PORT':process.env.PORT || 3001
 };
